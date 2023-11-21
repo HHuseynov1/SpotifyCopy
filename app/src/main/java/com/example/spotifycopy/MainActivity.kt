069320f4bc28f854f0272de8e216e9511e7bcfa8
Huseyn Huseynov <huseynhsynv004@gmail.com>
2023-11-21 21:03:53 +0400

    SpotifyCopy

--- app/src/main/java/com/example/spotifycopy/MainActivity.kt
+++ app/src/main/java/com/example/spotifycopy/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.spotifycopy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.spotifycopy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
         setContentView(R.layout.activity_main)
     }
 }
