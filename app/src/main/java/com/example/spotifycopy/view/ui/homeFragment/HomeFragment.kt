f7c84b2007f9b93d4d61d2829b10d6938a786fc1
Huseyn Huseynov <huseynhsynv004@gmail.com>
2023-12-15 14:25:27 +0400

    SpotifyCopy

--- app/src/main/java/com/example/spotifycopy/view/ui/homeFragment/HomeFragment.kt
+++ app/src/main/java/com/example/spotifycopy/view/ui/homeFragment/HomeFragment.kt
@@ -4,14 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.drawerlayout.widget.DrawerLayout.DrawerListener
 import androidx.fragment.app.Fragment
 import com.example.spotifycopy.R
+import com.example.spotifycopy.databinding.ActivityMainBinding
+import com.example.spotifycopy.databinding.FragmentHomeBinding
+
 class HomeFragment : Fragment() {
+
+    private lateinit var binding : FragmentHomeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+
+        binding.profileImage.setImageResource(R.drawable.spotifylogo)
+
+        binding.profileImage.setOnClickListener {
+        }
+
+        return binding.root
     }
 }
