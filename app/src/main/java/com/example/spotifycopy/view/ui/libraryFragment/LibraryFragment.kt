fb4ae495a6f5de96ca1153ff13073c0fc45116f0
Huseyn Huseynov <huseynhsynv004@gmail.com>
2023-12-22 14:06:28 +0400

    SpotifyCopy

--- app/src/main/java/com/example/spotifycopy/view/ui/libraryFragment/LibraryFragment.kt
+++ app/src/main/java/com/example/spotifycopy/view/ui/libraryFragment/LibraryFragment.kt
@@ -16,7 +16,7 @@ class LibraryFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentLibraryBinding.inflate(inflater,container,false)
 
         val mainActivity = activity as MainActivity
